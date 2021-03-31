082c583f8e774529fc20046e832fa61ddd9aa15d
hbs18 <fran.poje@gmail.com>
2021-03-31 12:04:26 +0200

    Portfolio screen rewrite.

--- app/src/main/java/cf/hbs18/fragmenttest/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/cf/hbs18/fragmenttest/ui/dashboard/DashboardFragment.kt
@@ -15,6 +15,7 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import cf.hbs18.fragmenttest.R
+import cf.hbs18.fragmenttest.format2
 import cf.hbs18.fragmenttest.jsonData
 import cf.hbs18.fragmenttest.network.EthApi
 import cf.hbs18.fragmenttest.network.MarsApi
@@ -24,6 +25,8 @@ import retrofit2.Callback
 import retrofit2.Response
 import java.math.BigDecimal
 import java.math.RoundingMode
+import java.text.NumberFormat
+import java.util.*
 
 
 class DashboardFragment : Fragment() {
@@ -50,121 +53,104 @@ class DashboardFragment : Fragment() {
     override fun onResume() {
         super.onResume()
         root.findViewById<TextView>(R.id.portfolioValue).text="0"
-        UpdateBTCPrice()
-        UpdateETHPrice()
-        updateCash()
-        //hideETHstuff()
-        //updateaj btc statsove, pa eth, pa onda cash (tvoj usd stats i portfolio value)
+        setBTC()
+        setETH()
+        setUSD()
+        calculatePortfolioValue()
     }
 
-    var attempts = 0
-    var bitcoin_price = 0.0
+    lateinit var format3: NumberFormat
 
-    var attemptsEth = 0
-    var eth_price = 0.0
-    fun UpdateBTCPrice() {
-        val walletPref = this.requireActivity().getSharedPreferences("wallet", Context.MODE_PRIVATE)
+    fun setBTC(){
+        format2 = NumberFormat.getInstance()
+        format2.setMinimumFractionDigits(2)
+        format2.setMaximumFractionDigits(2)
+
+        format3 = NumberFormat.getInstance()
+        format3.setMaximumFractionDigits(8)
+
+        val btcDataSharedPrefs: SharedPreferences = this.requireActivity().getSharedPreferences("btcData", AppCompatActivity.MODE_PRIVATE)
+        val walletSharedPrefs = this.requireActivity().getSharedPreferences("wallet", Context.MODE_PRIVATE)
+        val btcPrice = btcDataSharedPrefs.getString("currentPrice", "0")?.toFloat()
+        val yourBtc = walletSharedPrefs.getFloat("btc", 0F)
+        val yourbtcValueInUsd = yourBtc* btcPrice!!
+        //now set views
         root.findViewById<ProgressBar>(R.id.progressBarBTC).isVisible = false
-        MarsApi.retrofitService.getProperties().enqueue(
-                object : Callback<String> {
-                    override fun onFailure(call: Call<String>, t: Throwable) {
-                    }
-
-                    override fun onResponse(call: Call<String>, response: Response<String>) {
-                        val jsonShit = response.body().toString()
-                        val parsedJson = Gson().fromJson(jsonShit, jsonData::class.java)
-
-                        if (isAdded) {
-
-                            try {
-                                root.findViewById<ProgressBar>(R.id.progressBarBTC).isVisible = false
-                                //root.findViewById<TextView>(R.id.portfolioValue).text="0"
-                                root.findViewById<TextView>(R.id.tvAmountBTC).text = BigDecimal(((BigDecimal(parsedJson[0].price.toDouble()).setScale(2, RoundingMode.HALF_EVEN)) * walletPref.getFloat("btc", 0F).toBigDecimal()).toDouble()).setScale(2, RoundingMode.HALF_EVEN).toString()          //fetchani price puta kolicina btc iz sharedpreferences i formatting
-                                //root.findViewById<TextView>(R.id.tvAmountBTC).text = (parsedJson[0].price.toDouble() * walletPref.getFloat("btc", 0F)).toString()
-                                bitcoin_price = (BigDecimal(parsedJson[0].price.toDouble()).setScale(2, RoundingMode.HALF_EVEN)).toDouble()
-                                val totalPFValue = root.findViewById<TextView>(R.id.portfolioValue).text.toString().replace("$", "").toBigDecimal() + BigDecimal(((BigDecimal(parsedJson[0].price.toDouble()).setScale(2, RoundingMode.HALF_EVEN)) * walletPref.getFloat("btc", 0F).toBigDecimal()).toDouble()).setScale(2, RoundingMode.HALF_EVEN)
-                                //val totalPFValue = (parsedJson[0].price.toDouble() * walletPref.getFloat("btc", 0F)) + walletPref.getFloat("usd", 0F)
-                                root.findViewById<TextView>(R.id.portfolioValue).text = "$" + totalPFValue.toString()
-
-                                root.findViewById<TextView>(R.id.tvDateBTC).text = walletPref.getFloat("btc", 0F).toString() + " BTC"      //gets current btc you have and places it where appropriate
-
-                                root.findViewById<TextView>(R.id.tvNameBTC).text = "Bitcoin"
-                                root.findViewById<TextView>(R.id.text_view_dollarUSD).text = "$"
-                            } catch (e: NullPointerException) {
-                                if (attempts < 6) {
-                                    UpdateBTCPrice()
-                                    attempts++
-                                } else System.err.println("Null pointer exception");
-                                attempts = 0
-                            }
-
-
-                        }
-                    }
-                })
+        root.findViewById<TextView>(R.id.tvAmountBTC).text = format2.format(yourbtcValueInUsd)
+        root.findViewById<TextView>(R.id.tvDateBTC).text = format3.format(yourBtc).toString() + " BTC"
+        root.findViewById<TextView>(R.id.tvNameBTC).text = "Bitcoin"
+        root.findViewById<TextView>(R.id.text_view_dollarUSD).text = "$"
         return
     }
 
-    fun UpdateETHPrice() {
-        val walletPref = this.requireActivity().getSharedPreferences("wallet", Context.MODE_PRIVATE)
+    fun setETH(){
+        format2 = NumberFormat.getInstance()
+        format2.setMinimumFractionDigits(2)
+        format2.setMaximumFractionDigits(2)
+
+        format3 = NumberFormat.getInstance()
+        format3.setMaximumFractionDigits(8)
+
+        val ethDataSharedPrefs: SharedPreferences = this.requireActivity().getSharedPreferences("ethData", AppCompatActivity.MODE_PRIVATE)
+        val walletSharedPrefs = this.requireActivity().getSharedPreferences("wallet", Context.MODE_PRIVATE)
+        val ethPrice = ethDataSharedPrefs.getString("currentPrice", "0")?.toFloat()
+        val yourEth = walletSharedPrefs.getFloat("eth", 0F)
+        val yourethValueInUsd = yourEth* ethPrice!!
+        //now set views
         root.findViewById<ProgressBar>(R.id.progressBarEth).isVisible = false
-        EthApi.retrofitService.getProperties().enqueue(
-            object : Callback<String> {
-                override fun onFailure(call: Call<String>, t: Throwable) {
-                }
-
-                override fun onResponse(call: Call<String>, response: Response<String>) {
-                    val jsonShitEth = response.body().toString()
-                    val parsedJsonEth = Gson().fromJson(jsonShitEth, jsonData::class.java)
-
-                    if (isAdded) {
-
-                        try {
-                            root.findViewById<ProgressBar>(R.id.progressBarEth).isVisible = false
-                            //root.findViewById<TextView>(R.id.portfolioValue).text="0"
-                            root.findViewById<TextView>(R.id.tvAmountEth).text = BigDecimal(((BigDecimal(parsedJsonEth[0].price.toDouble()).setScale(2, RoundingMode.HALF_EVEN)) * walletPref.getFloat("eth", 0F).toBigDecimal()).toDouble()).setScale(2, RoundingMode.HALF_EVEN).toString()          //fetchani price puta kolicina eth iz sharedpreferences i formatting
-                            //root.findViewById<TextView>(R.id.tvAmountBTC).text = (parsedJson[0].price.toDouble() * walletPref.getFloat("btc", 0F)).toString()
-                            eth_price = (BigDecimal(parsedJsonEth[0].price.toDouble()).setScale(2, RoundingMode.HALF_EVEN)).toDouble()
-                            val totalPFValue = root.findViewById<TextView>(R.id.portfolioValue).text.toString().replace("$", "").toBigDecimal() + BigDecimal(((BigDecimal(parsedJsonEth[0].price.toDouble()).setScale(2, RoundingMode.HALF_EVEN)) * walletPref.getFloat("eth", 0F).toBigDecimal()).toDouble()).setScale(2, RoundingMode.HALF_EVEN)
-                            root.findViewById<TextView>(R.id.portfolioValue).text = "$" + totalPFValue.toString()
-
-                            root.findViewById<TextView>(R.id.tvDateEth).text = walletPref.getFloat("eth", 0F).toString() + " ETH"      //gets current btc you have and places it where appropriate
-
-                            root.findViewById<TextView>(R.id.tvNameEth).text = "Ethereum"
-                            root.findViewById<TextView>(R.id.text_view_dollarEth).text = "$"
-                        } catch (e: NullPointerException) {
-                            if (attempts < 6) {
-                                UpdateETHPrice()
-                                attempts++
-                            } else System.err.println("Null pointer exception");
-                            attempts = 0
-                        }
-
-
-                    }
-                }
-            })
+        root.findViewById<TextView>(R.id.tvAmountEth).text = format2.format(yourethValueInUsd)
+        root.findViewById<TextView>(R.id.tvDateEth).text = format3.format(yourEth).toString() + " ETH"
+        root.findViewById<TextView>(R.id.tvNameEth).text = "Ethereum"
+        root.findViewById<TextView>(R.id.text_view_dollarEth).text = "$"
         return
+
     }
 
-    fun updateCash(){
+    fun setUSD(){
+        format2 = NumberFormat.getInstance()
+        format2.setMinimumFractionDigits(2)
+        format2.setMaximumFractionDigits(2)
+
+        format3 = NumberFormat.getInstance()
+        format3.setMaximumFractionDigits(8)
+
         val walletPref = this.requireActivity().getSharedPreferences("wallet", Context.MODE_PRIVATE)
+        val yourMoney = walletPref.getFloat("usd", 0F)
+
         root.findViewById<ProgressBar>(R.id.progressBarUSD).isVisible=false
         root.findViewById<TextView>(R.id.tvNameUSD).text = "US Dollars"
         root.findViewById<TextView>(R.id.tvDateUSD).text = "Non-invested money"
-        root.findViewById<TextView>(R.id.tvAmountUSD).text = walletPref.getFloat("usd", 0F).toString()
+        root.findViewById<TextView>(R.id.tvAmountUSD).text = format2.format(yourMoney)
         root.findViewById<TextView>(R.id.text_view_dollarBTC).text="$"
-        val totalPFValue = root.findViewById<TextView>(R.id.portfolioValue).text.toString().replace("$", "").toFloat() + BigDecimal(walletPref.getFloat("usd", 0F).toDouble()).setScale(2, RoundingMode.HALF_EVEN).toFloat()
-        root.findViewById<TextView>(R.id.portfolioValue).text = "$"+totalPFValue.toString()
         return
     }
 
-    fun refresh(item: MenuItem){
-        root.findViewById<TextView>(R.id.portfolioValue).text="0"
-        UpdateBTCPrice()
-        updateCash()
+    fun calculatePortfolioValue(){
+        format2 = NumberFormat.getCurrencyInstance()
+        format2.setMinimumFractionDigits(2)
+        format2.setMaximumFractionDigits(2)
+        format2.setCurrency(Currency.getInstance("USD"))
+
+        val walletSharedPrefs = this.requireActivity().getSharedPreferences("wallet", Context.MODE_PRIVATE)
+        val ethDataSharedPrefs: SharedPreferences = this.requireActivity().getSharedPreferences("ethData", AppCompatActivity.MODE_PRIVATE)
+        val btcDataSharedPrefs: SharedPreferences = this.requireActivity().getSharedPreferences("btcData", AppCompatActivity.MODE_PRIVATE)
+
+        val btcPrice = btcDataSharedPrefs.getString("currentPrice", "0")?.toFloat()
+        val yourBtc = walletSharedPrefs.getFloat("btc", 0F)
+        val yourbtcValueInUsd = yourBtc* btcPrice!!
+
+        val ethPrice = ethDataSharedPrefs.getString("currentPrice", "0")?.toFloat()
+        val yourEth = walletSharedPrefs.getFloat("eth", 0F)
+        val yourMoney = walletSharedPrefs.getFloat("usd", 0F)
+        val yourethValueInUsd = yourEth* ethPrice!!
+
+        val PFValue = yourbtcValueInUsd+yourethValueInUsd+yourMoney
+
+        root.findViewById<TextView>(R.id.portfolioValue).text = format2.format(PFValue).toString()
+
         return
     }
+
 
     fun hideETHstuff(){
         root.findViewById<ConstraintLayout>(R.id.constraintlayoutETH).isVisible = false
